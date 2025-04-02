0f572603491fa7ee797796efd0c4e2e5457be6ee
Jordan Sibley <jordan.c.sibley@gmail.com>
2025-04-02 13:22:42 -0700

    Update documentation

--- assignment1-Sibley.R
+++ assignment1-Sibley.R
@@ -4,13 +4,15 @@
 
 ## Writing functions!  
 
-# This is a function that will solve for the energy produced from a photovoltaic system.
-# The values for panel yield (r) and performance ration (PR) are preset, 
-# and the inputs in the function will be solar panel area in m^2 (A) and 
-# annual average solar radiation in kWh. The function will then print the energy value in kWh. 
-
-
-# Write function 
+#' Photovoltaic system energy function
+#' @description
+#' This is a function that will solve for the energy produced from a photovoltaic system. The values for panel yield (r) and performance ration (PR) are preset, and the inputs in the function will be solar panel area in m^2 (A) and annual average solar radiation in kWh. The function will then print the energy value in kWh. 
+#'
+#' @param A 
+#' @param H 
+#'
+#' @returns Returns the energy produced in kWh within the photovoltaic system given the inputed parameters 
+#'
 energy <- function(A, H) {
   # Formula for energy produced from a photovolatic system 
   E <- A * 0.2 * H * 0.75  
